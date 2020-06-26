d6e48b007a9fb2221316952e3a2ee5bcf95924fc
bekzodnj <bekzodnx@gmail.com>
2020-06-26 13:47:41 +0100

    3. ReverseInt solution added

--- exercises/3. reverseint/index.js
+++ exercises/3. reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversed = 0;
+  let num_copy = n;
+
+  while (num_copy !== 0) {
+    reversed = reversed * 10 + (num_copy % 10);
+    num_copy = parseInt(num_copy / 10);
+  }
+
+  return reversed;
+}
 
 module.exports = reverseInt;
