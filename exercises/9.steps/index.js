a5031e936c6c591c15425855c0a4d933dd73cf74
bekzodnj <bekzodnx@gmail.com>
2020-07-04 23:15:55 +0100

    Added 9.steps solution

--- exercises/9.steps/index.js
+++ exercises/9.steps/index.js
@@ -17,6 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 1; i <= n; i++) {
+    let word = '';
+    let j;
+    for (j = 1; j <= i; j++) {
+      word += '#';
+    }
+    for (let k = 1; k <= n - j + 1; k++) {
+      word += ' ';
+    }
+    console.log(word);
+  }
+}
 
 module.exports = steps;
