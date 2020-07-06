dc527813c50b972ab1c646bde43580479cd5321e
bekzodnj <bekzodnx@gmail.com>
2020-07-06 22:10:04 +0100

    Added: 11.vowel counter solution

--- exercises/11.vowels/index.js
+++ exercises/11.vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowelCounter = 0;
+
+  for (let char of str) {
+    if (isVowelRegEx(char)) vowelCounter++;
+  }
+
+  return vowelCounter;
+}
+
+function isVowelRegEx(char) {
+  return /[aeiouAEIOU]/.test(char);
+}
 
 module.exports = vowels;
