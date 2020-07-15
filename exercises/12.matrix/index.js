429af0b15027338e799ce280f445b9b8e24ecd97
bekzodnj <bekzodnx@gmail.com>
2020-07-15 12:10:49 +0100

    Return NxN spiral-matrix

--- exercises/12.matrix/index.js
+++ exercises/12.matrix/index.js
@@ -16,50 +16,49 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-  const result = [];
+  const results = [];
+
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
 
   let counter = 1;
-
   let startRow = 0;
   let endRow = n - 1;
   let startCol = 0;
   let endCol = n - 1;
 
-  for (let i = 0; i < n; i++) {
-    result.push([]);
-  }
-
-  while (startRow <= endRow && startCol <= startCol) {
+  while (startRow <= endRow && startCol <= endCol) {
     // top side
     for (let i = startCol; i <= endCol; i++) {
-      result[startRow][i] = counter;
+      results[startRow][i] = counter;
       counter++;
     }
     startRow++;
 
     // right side
     for (let i = startRow; i <= endRow; i++) {
-      result[i][endRow] = counter;
+      results[i][endRow] = counter;
       counter++;
     }
     endCol--;
 
     // bottom side
     for (let i = endCol; i >= startCol; i--) {
-      result[endRow][i] = counter;
+      results[endRow][i] = counter;
       counter++;
     }
     endRow--;
 
-    // left col
+    // start col
     for (let i = endRow; i >= startRow; i--) {
-      result[i][startCol] = counter;
+      results[i][startCol] = counter;
       counter++;
     }
     startCol++;
   }
 
-  return result;
+  return results;
 }
 
 module.exports = matrix;
