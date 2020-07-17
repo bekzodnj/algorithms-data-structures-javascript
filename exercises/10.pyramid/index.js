233e3456517f0d8fc1ab148b20ccaf263b239529
bekzodnj <bekzodnx@gmail.com>
2020-07-17 22:59:50 +0100

    Initital solution complex pyramid (10)

--- exercises/10.pyramid/index.js
+++ exercises/10.pyramid/index.js
@@ -14,6 +14,33 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let spaces = n - 1;
+  let hashes = 1;
+
+  //loop for lines of output
+  for (let i = 0; i < n; i++) {
+    let output = '';
+
+    //spaces adder starting
+    for (let i = 0; i < spaces; i++) {
+      output += ' ';
+    }
+
+    // # (hash) addeer
+    for (let i = 0; i < hashes; i++) {
+      output += '#';
+    }
+
+    //spaces adder ending
+    for (let i = 0; i < spaces; i++) {
+      output += ' ';
+    }
+    console.log(output);
+
+    spaces--;
+    hashes = hashes * 2 - 1;
+  }
+}
 
 module.exports = pyramid;
